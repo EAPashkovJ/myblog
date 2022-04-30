68e4ce3c48e2c6ef25eb1acebcb79abce86037aa
Evgenii <eapashkov@gmail.com>
2022-04-30 19:17:52 +0300

    added captcha auth

--- src/main/resources/db/migration/V2__Add_admin.sql
+++ src/main/resources/db/migration/V2__Add_admin.sql
@@ -1,5 +1,6 @@
 insert into usr (id, username, password, active)
 values (1, 'admin', '123', true);
+# 202cb962ac59075b964b07152d234b70
 
 insert into user_role (user_id, roles)
 values (1, 'USER'), (1, 'ADMIN');
